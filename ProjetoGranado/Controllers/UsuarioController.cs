13aa882e868803db12287bdb2ce4a67f6ce557aa
kadujs82 <kadujs@hotmail.com>
2023-10-25 15:26:15 -0300

    Commit Projeto Granaddo

---
+++ ProjetoGranado/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoGranado.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
