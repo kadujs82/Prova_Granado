19e1b8354fff576756ba2f7aed8e17c010e088d5
kadujs82 <kadujs@hotmail.com>
2023-10-26 10:14:41 -0300

    Adicionando o EntityFramework

---
+++ ProjetoGranado/Models/Setor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoGranado.Models
+{
+    public class Setor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+        [Required]
+        public string Nome { get; set; }
+    }
+}
---
+++ ProjetoGranado/Models/Usuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoGranado.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+        [Required]
+        public string Nome { get; set; }
+
+        [Key]
+        [MaxLength(11)]
+        public string Cpf { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(1)]
+        public string Ativo { get; set; }
+
+        [MaxLength(1)]
+        public string Sexo { get; set; }
+
+        [MaxLength(11)]
+        public string Telefone { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public List<Setor> Setor { get; set; }
+        public List<GrupoUsuarios> GrupoUsuarios { get; set; }
+
+        
+    }
+}
