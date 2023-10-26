5a90499e6b2824689c6499adc5802d500e32ed8e
kadujs82 <kadujs@hotmail.com>
2023-10-26 15:07:41 -0300

    Alteraçao tela de login

--- ProjetoGranado/Startup.cs
+++ ProjetoGranado/Startup.cs
@@ -51,7 +51,7 @@ namespace ProjetoGranado
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
